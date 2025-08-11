2bfc232cade0d0dd65c2b57a4283033561d5f7b3
robertke <robert_kerkhoven@hotmail.com>
2025-08-11 17:34:34 +0200

    additional tests making use of the lombok methods

--- src/test/java/org/linkedin/taskmanager/repository/TaskRepositoryTest.java
+++ src/test/java/org/linkedin/taskmanager/repository/TaskRepositoryTest.java
@@ -5,6 +5,7 @@ import org.linkedin.taskmanager.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,31 +19,64 @@ public class TaskRepositoryTest {
     @Test
     void TestSaveTask(){
         //arrange    given
-        Task task = new Task();
-        task.setTitle("TestTask");
-        task.setStatus("To do");
-
+        Task task = new Task("TestTask", "To do");
         //act   when
-
         Task savedTask = taskRepository.save(task);
-
         //assert   then
         assertNotNull(savedTask);
         assertEquals("TestTask", task.getTitle());
-
     }
 
     @Test
     void testDeleteTask(){
         //arrange
-        Task task = new Task();
-        task.setTitle("Task to delete");
-        task.setStatus("Done");
+        Task task = new Task("Task to delete", "Done");
         taskRepository.save(task);
         //act
         taskRepository.delete(task);
         Optional<Task> deletedTask = taskRepository.findById(task.getId());
         //assert
         assertFalse(deletedTask.isPresent());
+    }
+
+    @Test
+    void TestFindTaskById(){
+        //arrange  given
+        Task task = new Task("TestTask", "To do");
+        taskRepository.save(task);
+        //act   when
+        Optional<Task> optionalTask = taskRepository.findById(task.getId());
+        //assert   then
+        assertFalse(optionalTask.isEmpty());
+        assertEquals(task.getId(), optionalTask.get().getId());
+    }
+
+    @Test
+    void testUpdateTaskStatus(){
+        //arrange  given
+        Task task = new Task("TestTask", "To do");
+        taskRepository.save(task);
+        //act   when
+        task.setStatus("Done");
+        taskRepository.save(task);
+        Optional<Task> optionalTask = taskRepository.findById(task.getId());
+        //assert   then
+        assertFalse(optionalTask.isEmpty());
+        assertEquals("Done", optionalTask.get().getStatus());
+    }
+
+
+    @Test
+    void testFindAllTasks() {
+        // arrange
+        Task task1 = new Task("Task 1", "To do");
+        taskRepository.save(task1);
+        Task task2 = new Task("Task 2", "Done");
+        taskRepository.save(task2);
+        // act
+        List<Task> tasks = taskRepository.findAll();
+        // assert
+        assertEquals(2, tasks.size());
+
     }
 }
